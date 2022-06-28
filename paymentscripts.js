6fc2baace72c4fe234ab9e478bcf22c8188d6e3b
BoatyJra <boatwin18@gmail.com>
2022-06-28 19:15:22 +0700

    change button jquery

---
+++ paymentscripts.js
@@ -0,0 +1,79 @@
+$(document).click(function (e) {
+  if (e.target.id != "success-outlined1") {
+    document.getElementById("changeClass1").classList.remove("customred");
+    console.log(e.target.id);
+  }
+  console.log("outside if");
+});
+
+$(document).click(function (e) {
+  if (e.target.id != "success-outlined2") {
+    document.getElementById("changeClass2").classList.remove("customred");
+    console.log(e.target.id);
+  }
+  console.log("outside if");
+});
+
+$(document).click(function (e) {
+  if (e.target.id != "success-outlined3") {
+    document.getElementById("changeClass3").classList.remove("customred");
+    console.log(e.target.id);
+  }
+  console.log("outside if");
+});
+
+$(document).click(function (e) {
+  if (e.target.id != "success-outlined4") {
+    document.getElementById("changeClass4").classList.remove("customred");
+    console.log(e.target.id);
+  }
+  console.log("outside if");
+});
+
+$(document).click(function (e) {
+  if (e.target.id != "success-outlined5") {
+    document.getElementById("changeClass5").classList.remove("customred");
+    console.log(e.target.id);
+  }
+  console.log("outside if");
+});
+
+$(document).click(function (e) {
+  if (e.target.id != "success-outlined6") {
+    document.getElementById("changeClass6").classList.remove("customred");
+    console.log(e.target.id);
+  }
+  console.log("outside if");
+});
+
+$(document).click(function (e) {
+  if (e.target.id != "success-outlined7") {
+    document.getElementById("changeClass7").classList.remove("customred");
+    console.log(e.target.id);
+  }
+  console.log("outside if");
+});
+
+$(document).click(function (e) {
+  if (e.target.id != "success-outlined8") {
+    document.getElementById("changeClass8").classList.remove("customred");
+    console.log(e.target.id);
+  }
+  console.log("outside if");
+});
+
+$(document).click(function (e) {
+  if (e.target.id != "success-outlined9") {
+    document.getElementById("changeClass9").classList.remove("customred");
+    console.log(e.target.id);
+  }
+  console.log("outside if");
+});
+
+$(document).click(function (e) {
+  if (e.target.id != "success-outlined10") {
+    document.getElementById("changeClass10").classList.remove("customred");
+    console.log(e.target.id);
+  }
+  console.log("outside if");
+});
